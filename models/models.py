0712efec5bc65aa9262c83ad9a8a4d6840458a67
Aschull <andrews.fernandesx@gmail.com>
2021-10-10 22:24:39 -0300

    Retirado imports desnecessários

--- models/models.py
+++ models/models.py
@@ -1,5 +1,5 @@
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Float, DateTime
-from sqlalchemy.orm import scoped_session, sessionmaker, relationship
+from sqlalchemy import create_engine, Column, Integer, String, Float, DateTime
+from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import datetime
 
