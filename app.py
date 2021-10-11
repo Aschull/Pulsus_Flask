4d3b390ebfab78da379a8d7a16bfd29a36d7596a
Aschull <andrews.fernandesx@gmail.com>
2021-10-10 22:26:26 -0300

    Retirado BasicAuth

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
 from flask import Flask
 from flask_restful import Api
-from flask_httpauth import HTTPBasicAuth
-
 from models.models import init_db
 from resources.device import Device
 from resources.device import InsereDevice
 from resources.device import ListaDevices
 
-auth = HTTPBasicAuth()
 app = Flask(__name__)
 api = Api(app)
-
 
 api.add_resource(Device, '/device/<int:id>/')
 api.add_resource(InsereDevice, '/insereDevice/')
